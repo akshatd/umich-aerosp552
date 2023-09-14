715a0ce38b3637298a68a003d5ffc2c2c6b31702
Akshat Dubey <akshat@transcelestial.com>
2023-09-14 03:58:02 -0400

    fix: convert ints to chars for easer reading

--- hw1/life.cpp
+++ hw1/life.cpp
@@ -6,73 +6,70 @@
 #define kDeadCell  ' '
 #define kQuitInput "quit"
 
-constexpr int kNumFiles = 1;
-constexpr int kMaxRows  = 1000;
-constexpr int kMaxCols  = 1000;
+// constexpr int kNumFiles = 5;
+constexpr int kMaxRows = 1000;
+constexpr int kMaxCols = 1000;
 
-const std::string kInitialPatternFiles[] = {
-	"life.dat",
-};
+const std::string kInitialPattern("life_beacon.dat");
+// const std::string kInitialPatternFiles[] = {
+// 	"life.dat", "life1.dat", "life_block.dat", "life_beehive.dat", "life_beacon.dat",
+// };
 
-void  playGameOfLife(std::string pattern_filename);
-int **readFile(std::string pattern_filename, bool &success, int &rows, int &cols);
-int   getNumNbrs(int **data, int rows, int cols, int row_idx, int col_idx);
+void   playGameOfLife(std::string pattern_filename);
+char **readFile(std::string pattern_filename, bool &success, int &rows, int &cols);
+int    getNumNbrs(char **data, int rows, int cols, int row_idx, int col_idx);
 
 int main(void) {
-	for (int i = 0; i < kNumFiles; ++i) {
-		playGameOfLife(kInitialPatternFiles[i]);
-	}
+	// for (int i = 0; i < kNumFiles; ++i) {
+	// 	playGameOfLife(kInitialPatternFiles[i]);
+	// }
+	playGameOfLife(kInitialPattern);
 }
 
 void playGameOfLife(std::string pattern_filename) {
-	std::cout << "Using " << pattern_filename << " as initial pattern \n";
-	int   rows, cols;
-	bool  success;
-	int **data      = readFile(pattern_filename, success, rows, cols);
-	int **data_next = new int *[rows];
+	// std::cout << "Using " << pattern_filename << " as initial pattern \n";
+	int    rows, cols;
+	bool   success;
+	char **data = readFile(pattern_filename, success, rows, cols);
+	if (!success) return;
+
+	char **data_next = new char *[rows];
 	for (int row = 0; row < rows; ++row) {
-		data_next[row] = new int[cols];
+		data_next[row] = new char[cols];
 		for (int col = 0; col < cols; ++col) {
 			data_next[row][col] = data[row][col];
 		}
 	}
-	if (!success) return;
-	// check the data
-	// for (int i = 0; i < rows; ++i) {
-	// 	for (int j = 0; j < cols; ++j) {
-	// 		std::cout << data[i][j];
-	// 	}
-	// 	std::cout << '\n';
-	// }
+
 	bool        can_continue = true;
 	std::string user_input;
 	while (can_continue) {
+		for (int row = 0; row < rows; ++row) {
+			for (int col = 0; col < rows; ++col) {
+				int neighbours = getNumNbrs(data, rows, cols, row, col);
+				// std::cout << row << ',' << col << " has " << neighbours << " neighbours\n";
+				if (neighbours < 2) {
+					data_next[row][col] = kDeadCell;
+				} else if (neighbours == 2 && data[row][col] == kAliveCell) {
+					data_next[row][col] = kAliveCell;
+				} else if (neighbours == 3) {
+					data_next[row][col] = kAliveCell;
+					// } else if (neighbours >= 4) {
+					// 	data_next[row][col] = kDeadCell;
+				} else {
+					data_next[row][col] = kDeadCell;
+				}
+			}
+		}
+		char **data_temp = data;
+		data             = data_next;
+		data_next        = data_temp;
 		for (int row = 0; row < rows; ++row) {
 			for (int col = 0; col < cols; ++col) {
 				std::cout << data[row][col];
 			}
 			std::cout << '\n';
 		}
-		for (int row = 0; row < rows; ++row) {
-			for (int col = 0; col < rows; ++col) {
-				int neighbours = getNumNbrs(data, rows, cols, row, col);
-				// std::cout << row << ',' << col << " has " << neighbours << " neighbours\n";
-				if (neighbours < 2) {
-					data_next[row][col] = 0;
-				} else if (neighbours == 2 && data[row][col]) {
-					data_next[row][col] = 1;
-				} else if (neighbours == 3) {
-					data_next[row][col] = 1;
-					// } else if (neighbours >= 4) {
-					// 	data_next[row][col] = 0;
-				} else {
-					data_next[row][col] = 0;
-				}
-			}
-		}
-		int **data_temp = data;
-		data            = data_next;
-		data_next       = data_temp;
 		std::cout << "Press <Enter> to continue or type quit\n";
 		std::getline(std::cin, user_input);
 		if (user_input.size() != 0 && user_input == kQuitInput) { // user hit enter, continue
@@ -81,11 +78,13 @@ void playGameOfLife(std::string pattern_filename) {
 	}
 	for (int row = 0; row < rows; ++row) {
 		delete data[row];
+		delete data_next[row];
 	}
 	delete data;
+	delete data_next;
 }
 
-int **readFile(std::string pattern_filename, bool &success, int &rows, int &cols) {
+char **readFile(std::string pattern_filename, bool &success, int &rows, int &cols) {
 	std::ifstream input_file(pattern_filename);
 	if (!input_file.is_open()) {
 		std::cout << "** Error: Could not open " << pattern_filename << '\n';
@@ -119,19 +118,19 @@ int **readFile(std::string pattern_filename, bool &success, int &rows, int &cols
 
 	// put data in data struct
 	std::ifstream input_file_again(pattern_filename);
-	int         **temp_data = new int *[rows];
+	char        **temp_data = new char *[rows];
 	int           line_idx  = 0;
 	for (std::string line; std::getline(input_file_again, line); ++line_idx) {
-		temp_data[line_idx] = new int[cols];
+		temp_data[line_idx] = new char[cols];
 		for (int j = 0; j < cols; ++j) {
-			temp_data[line_idx][j] = (line[j] == kAliveCell);
+			temp_data[line_idx][j] = line[j];
 		}
 	}
 	success = true;
 	return temp_data;
 }
 
-int getNumNbrs(int **data, int rows, int cols, int row_idx, int col_idx) {
+int getNumNbrs(char **data, int rows, int cols, int row_idx, int col_idx) {
 	int row_low    = std::max(0, row_idx - 1);
 	int row_high   = std::min(rows - 1, row_idx + 1);
 	int col_low    = std::max(0, col_idx - 1);
@@ -140,7 +139,7 @@ int getNumNbrs(int **data, int rows, int cols, int row_idx, int col_idx) {
 	for (int row = row_low; row <= row_high; ++row) {
 		for (int col = col_low; col <= col_high; ++col) {
 			if (row == row_idx && col == col_idx) continue;
-			if (data[row][col] == 1) ++neighbours;
+			if (data[row][col] == kAliveCell) ++neighbours;
 		}
 	}
 	return neighbours;
